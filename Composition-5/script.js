bb7c0f1469fd881097df66d93b78764e276cabcb
Won Kyung Jung <jungw489@newschool.edu>
2016-12-13 11:03:40 +0000

    new catalog

--- Composition-5/script.js
+++ Composition-5/script.js
@@ -1,24 +1,29 @@
 $("body").css("background-color","white");
 
-for (var i=0; i < 60; i++) {
+for (var i=0; i < 50; i++) {
     $("body").append("<div class='dot'></div>");
 }
 
 var visibility = 0;
 var myCounter = 1;
 
-var heart = ["heart.png", "heart-2.png", "heart-3.png"];
+var variety = ["chocolate.png", "gum.png", "lollipop.png", "skittles.jpg"];
 
-$("body").append("<img src='" + heart[Math.floor(Math.random()*3)] + "'>");
-
+$("body").append("<img src='" + variety[Math.floor(Math.random()*4)] + "'>");
+   $(this).css("height",myCounter+100);
+   $(this).css("width",myCounter+100);
+   $(this).css("top",Math.floor(Math.random()*$(window).height()-100));
+   $(this).css("left",Math.floor(Math.random()*$(window).width()-100));
+   $(this).css("font-size",Math.floor(Math.random()*50));
+        
 $(".dot").each(function() {
    $(this).css("opacity", visibility/30);
    $(this).html(myCounter);
    $(this).css("height",myCounter+100);
    $(this).css("width",myCounter+100);
-   $(this).css("top",Math.floor(Math.random()*$(window).height()));
-   $(this).css("left",Math.floor(Math.random()*$(window).width()));
-   $(this).css("font-size",Math.floor(Math.random()*$(window).width()));
+   $(this).css("top",Math.floor(Math.random()*$(window).height()-300));
+   $(this).css("left",Math.floor(Math.random()*$(window).width()-200));
+   $(this).css("font-size",Math.floor(Math.random()*100));
    myCounter++;
    visibility++;
    
